40e7becb5eeca0cf7b1a626580361523d3602c85
Ariel Fogel <fogeltine@gmail.com>
2018-02-13 15:10:38 -0800

    Add authorization into app logic

--- project/forms.py
+++ project/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import Form
-from wtforms import StringField, DateField, IntegerField, SelectField
-from wtforms.validators import DataRequired 
+from wtforms import StringField, DateField, IntegerField, SelectField, \
+	PasswordField
+from wtforms.validators import DataRequired, Length
 
 class AddTaskForm(Form):
 	task_id = IntegerField()
@@ -19,3 +20,21 @@ class AddTaskForm(Form):
 			]
 		)
 	status = IntegerField('Status')
+
+class RegistrationForm(Form):
+	name = StringField('Username', validators=[
+		DataRequired(), Length(min=6, max=25)
+	])
+	email = StringField('Email', validators=[
+		DataRequired(), Length(min=6, max=40)
+	])
+	password = PasswordField('Password', validators = [
+		DataRequired(), Length(min=6, max=40)
+	])
+	confirm = PasswordField('Confirm Password', validators = [
+		DataRequired(), Length(min=6, max=40)
+	])
+
+class LoginForm(Form):
+	name = StringField('Username', validators=[DataRequired()])
+	password = StringField('Password', validators=[DataRequired()])
--- project/views.py
+++ project/views.py
@@ -8,29 +8,52 @@ app.config.from_object('_config') #neat, I should probably look at docs for this
 db = SQLAlchemy(app)
 
 # Import custom dependencies
-from forms import AddTaskForm
+from forms import AddTaskForm, RegistrationForm, LoginForm
 from helpers import login_required
-from models import Task
+from models import Task, Account
 
 # Route handlers
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+	error = None
+	form = LoginForm(request.form)
+	if request.method == 'POST':
+		if form.validate_on_submit():
+			account = Account.query.filter_by(name=form.name.data).first()
+			if account is not None and account.password == form.password.data:
+				session['logged_in'] = True
+				flash('Welcome!')
+				return redirect(url_for('tasks'))
+			else:
+				error = 'Invalid credentials. Please try again.'
+		else:
+			error = 'Both fields are required.'
+	return render_template('login.html', form=form, error=error)
+
 @app.route('/logout')
 def logout():
 	session.pop('logged_in', None)
 	flash('Goodbye!')
 	return redirect(url_for('login'))
 
-@app.route('/', methods=['GET', 'POST'])
-def login():
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	error = None
+	form = RegistrationForm(request.form)
+	print(form.data)
 	if request.method == 'POST':
-		if request.form['username'] != app.config['USERNAME'] \
-				or request.form['password'] != app.config['PASSWORD']:
-			error = 'Invalid credentials. Please try again.'
-			return render_template('login.html', error=error)
-		else:
-			session['logged_in'] = True
-			flash('Welcome!')
-			return redirect(url_for('tasks'))
-	return render_template('login.html')
+		if form.validate_on_submit():
+			new_account = Account(
+				form.name.data,
+				form.email.data,
+				form.password.data
+			) # need to encrypt password
+			db.session.add(new_account)
+			db.session.commit()
+			flash('Thanks for registering. Please login.')
+			return redirect(url_for('login'))
+	return render_template('register.html', form=form, error=error)
 
 @app.route('/tasks/')
 @login_required
