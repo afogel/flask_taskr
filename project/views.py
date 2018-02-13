a26a5350cd22695b84990dc0af0f9a7bc0ecfc12
Ariel Fogel <fogeltine@gmail.com>
2018-02-13 11:41:53 -0800

    correct syntax errors within routes, make types conform

--- project/views.py
+++ project/views.py
@@ -85,7 +85,7 @@ def new_task():
 @login_required
 def complete(task_id):
 	g.db = connect_db()
-	g.db.execute('UPDATE tasks SET status = 0 WHERE task_id = ?', task_id)
+	g.db.execute('UPDATE tasks SET status = 0 WHERE task_id = ?', str(task_id))
 	g.db.commit()
 	g.db.close()
 	flash('The task was marked as complete.')
@@ -95,7 +95,7 @@ def complete(task_id):
 @login_required
 def delete_entry(task_id):
 	g.db = connect_db()
-	g.db.execute('DELETE tasks SET status = 0 WHERE task_id = ?', task_id)
+	g.db.execute('DELETE FROM tasks WHERE task_id = ?', str(task_id))
 	g.db.commit()
 	g.db.close()
 	flash('The task was deleted.')
